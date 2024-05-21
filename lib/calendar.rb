9a124197f456bae287431362df6579ae15874080
rduthel <34297235+rduthel@users.noreply.github.com>
2024-05-21 17:29:16 +0200

    refactor: move select

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -27,10 +27,12 @@ def hourly_ranges(date, step)
   result
 end
 
-def after_current_slot_and_before_next_slot(slot:, end_of_busy_slot:, start_of_next_busy_slot:)
-  after_current_slot = slot["start"] >= end_of_busy_slot
-  before_next_slot = slot["end"] <= start_of_next_busy_slot
-  after_current_slot && before_next_slot
+def after_current_slot_and_before_next_slot(slots_of_day:, end_of_busy_slot:, start_of_next_busy_slot:)
+  slots_of_day.select do |slot|
+    after_current_slot = slot["start"] >= end_of_busy_slot
+    before_next_slot = slot["end"] <= start_of_next_busy_slot
+    after_current_slot && before_next_slot
+  end
 end
 
 def after_current_slot_and_before_end_of_day(slot:, end_of_day:, end_of_busy_slot:)
@@ -70,7 +72,7 @@ def free_slots(busy_calendar, step)
         search_before_start_of_day = true
         redo
       elsif next_slot
-        selection = slots_of_day.select { |slot| after_current_slot_and_before_next_slot(slot:, end_of_busy_slot: busy_slot.end, start_of_next_busy_slot: next_slot.start) }
+        selection = after_current_slot_and_before_next_slot(slots_of_day:, end_of_busy_slot: busy_slot.end, start_of_next_busy_slot: next_slot.start)
       elsif end_of_slot_before_end_of_day
         selection = slots_of_day.select { |slot| after_current_slot_and_before_end_of_day(slot:, end_of_day: date_at(day, END_OF_DAY), end_of_busy_slot: busy_slot.end) }
       else
