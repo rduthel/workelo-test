8388daf4cacde35b7a0dc0961edbe300dc31e89a
rduthel <34297235+rduthel@users.noreply.github.com>
2024-05-21 16:59:06 +0200

    refactor: class Slot

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -51,14 +51,16 @@ end
 
 def free_slots(busy_calendar, step)
   result = []
-  busy_calendar_day_by_day = busy_calendar.group_by { |slot| Time.new(slot["start"]).to_date }
+  busy_calendar_day_by_day = busy_calendar
+    .map { |calendar| Slot.new(calendar["start"], calendar["end"]) }
+    .group_by { |slot| slot.start.to_date }
 
   busy_calendar_day_by_day.each_pair do |day, busy_slots_of_day|
     search_before_start_of_day = false
     slots_of_day = hourly_ranges(day, step)
     busy_slots_of_day.each_with_index do |busy_slot, index|
-      start_of_busy_slot = Time.new(busy_slot["start"])
-      end_of_busy_slot = Time.new(busy_slot["end"])
+      start_of_busy_slot = busy_slot.start
+      end_of_busy_slot = busy_slot.end
       first_slot_before_start_of_day = index.zero? && start_of_busy_slot.hour > START_OF_DAY
       next_slot = busy_slots_of_day[index + 1]
       end_of_slot_before_end_of_day = end_of_busy_slot.hour < END_OF_DAY
@@ -67,12 +69,12 @@ def free_slots(busy_calendar, step)
         search_before_start_of_day = true
         redo
       elsif next_slot
-        start_of_next_busy_slot = Time.new(next_slot["start"])
+        start_of_next_busy_slot = next_slot.start
         result.push(slots_of_day.select { |calendar_slot| after_current_slot_and_before_next_slot(calendar_slot, end_of_busy_slot, start_of_next_busy_slot) })
       elsif end_of_slot_before_end_of_day
         result.push(slots_of_day.select { |calendar_slot| after_current_slot_and_before_end_of_day(calendar_slot, day, end_of_busy_slot) })
       else
-        end_of_previous_slot = Time.new(busy_slots_of_day[index - 1]["end"])
+        end_of_previous_slot = busy_slots_of_day[index - 1].end
         result.push(slots_of_day.select { |calendar_slot| after_current_slot_or_between_previous_and_current(calendar_slot, end_of_busy_slot, start_of_busy_slot, end_of_previous_slot) })
       end
     end
@@ -83,4 +85,13 @@ end
 
 def common_free_slots(first_calendar, second_calendar, step)
   free_slots(first_calendar, step) & free_slots(second_calendar, step)
+end
+
+class Slot
+  attr_accessor :start, :end
+
+  def initialize(start_of_slot, end_of_slot)
+    @start = Time.new(start_of_slot)
+    @end = Time.new(end_of_slot)
+  end
 end
